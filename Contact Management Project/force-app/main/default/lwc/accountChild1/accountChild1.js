6462b44b66d5cacdb7fb66fa6e19d3c0fc7b8b53
Kunal <kunal.bhardwaj2308@gmail.com>
2024-12-04 01:33:55 +0530

    commit

---
+++ Contact Management Project/force-app/main/default/lwc/accountChild1/accountChild1.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountChild1 extends LightningElement {}
---
+++ Contact Management Project/force-app/main/default/lwc/accountChild2/accountChild2.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountChild2 extends LightningElement {}
